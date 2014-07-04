2d04d3d17e677564a06b2dd7c613ad54c8aa38a0
pv <pat@cs.umass.edu>
2014-07-04 12:18:24 -0400

    poop

--- src/main/scala/co/pemma/Regexer.scala
+++ src/main/scala/co/pemma/Regexer.scala
@@ -38,7 +38,9 @@ class Regexer(thing1: String, thing2: String)
     val anyWordsRegex = ".*"
     val thingRegEx = s"(?:^$thing |(?:.* $thing\\W.*)|(?:.* $thing$$))"
 
-    io.Source.fromURL(patternUrl).getLines.filterNot(_.startsWith("#")).filter(_ != "")
+    val lines = io.Source.fromURL(patternUrl).getLines.toArray
+
+    lines.filter(!_.startsWith("#")).filter(_ != "")
       .map(pattern => s"($anyWordsRegex$pattern$thingRegEx)|($thingRegEx$pattern$anyWordsRegex)")
       .mkString("|").r
   }
