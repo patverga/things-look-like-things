7680b602ad42816a4fd739faf8c3386f81c57238
Patrick Verga <pat@sydney.cs.umass.edu>
2014-07-16 16:23:16 -0400

    wrong arg for mvn

--- bash/qsub-job.sh
+++ bash/qsub-job.sh
@@ -6,4 +6,4 @@ echo $ARGS
 
 cd /home/pat/things-look-like-things/
 
-mvn compile && mvn scala:run -DmainClass=co.pemma.MainThings -Dargs="${ARGS}"
+mvn compile && mvn scala:run -DmainClass=co.pemma.MainThings -DaddArgs="${ARGS} "
